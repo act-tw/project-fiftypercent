ec8902a840ca98679e6663ca2d1f15af5263d633
tooto1985 <tooto1985@gmail.com>
2015-10-15 13:35:53 +0800

    update

--- add.js
+++ add.js
@@ -1,7 +1,7 @@
 $(function() {
     QueryString("true") === "true" && (function() {
-    	$("form[name='formSc']").css("display","inline");
     	$("form[name='formSc']").find(">span:first").css("margin-left",10);
+    	$("form[name='formSc']").css("display","inline");
     	$("form[name='formSc']").appendTo($("#middle_option"));
     	$("form[name='formSc']").find(">img,>a").appendTo($("#footer_wrapper"));
 
