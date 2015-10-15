e3430295dd0282f966b5cc2494901385ef3afdcf
tooto1985 <tooto1985@gmail.com>
2015-10-15 13:33:15 +0800

    update

--- add.js
+++ add.js
@@ -3,6 +3,7 @@ $(function() {
     	$("form[name='formSc']").css("display","inline");
     	$("form[name='formSc']").appendTo($("#middle_option"));
     	$("form[name='formSc']").find(">img,>a").appendTo($("#footer_wrapper"));
+    	$("form[name='formSc']").find(">span:first").css("margin-left",10);
     	
 
     	console.log("true");
