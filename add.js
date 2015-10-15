abdfec72f1cadab7d128fd6470d85a4db4c884da
tooto1985 <tooto1985@gmail.com>
2015-10-15 13:22:30 +0800

    update

--- add.js
+++ add.js
@@ -1,5 +1,9 @@
 $(function() {
-    QueryString("test") === "true" && (function() {
-    	console.log("add.js");
+    QueryString("true") === "true" && (function() {
+    	$("form[name='formSc']").appendTo($("#middle_option"));
+    	$("form[name='formSc']").find(">img,>a").appendTo($("#footer_wrapper"));
+    	
+
+    	console.log("true");
     }());
 });
