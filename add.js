cebbe73db63bf84e8f2f49bd055bd769c3e4f81e
tooto1985 <tooto1985@gmail.com>
2015-10-27 10:11:58 +0800

    modify

--- add.js
+++ add.js
@@ -1,9 +1,9 @@
 $(function() {
-    QueryString("true") === "true" && (function() {
+    //QueryString("true") === "true" && (function() {
         $("form[name='formSc']").find(">span:first").css("margin-left", 10);
         $("form[name='formSc']").css("display", "inline");
         $("form[name='formSc']").appendTo($("#middle_option"));
         $("form[name='formSc']").find(">img,>a").appendTo($("#footer_wrapper"));
         $("#item_desc,#merDetail").show();
-    }());
+    //}());
 });
